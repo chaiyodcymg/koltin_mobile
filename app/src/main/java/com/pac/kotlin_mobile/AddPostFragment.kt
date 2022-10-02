18f154d1ea79ff665bd2d6127b81fa44e9f5252b
ornin  phongphaew <orain.p@kkumail.com>
2022-10-03 00:12:44 +0700

    addpost

--- app/src/main/java/com/pac/kotlin_mobile/AddPostFragment.kt
+++ app/src/main/java/com/pac/kotlin_mobile/AddPostFragment.kt
@@ -6,8 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.pac.kotlin_mobile.databinding.FragmentAddPostBinding
-import com.pac.kotlin_mobile.databinding.FragmentHomeBinding
+import com.pac.kotlin_mobile.databinding.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,12 +24,26 @@ class AddPostFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentAddPostBinding.inflate(layoutInflater)
-    binding.btnImageSelect.setOnClickListener {
-        Toast.makeText ( activity?.applicationContext,"Hello", Toast.LENGTH_LONG).show()
-    }
+        binding.findHouse.setOnClickListener(){
+            var frag : Fragment? = null
+            frag = AddCatFindhouse()
+            replaceFragment(frag)
+        }
+        binding.findCat.setOnClickListener{
+            var fragment : Fragment? = null
+            fragment = LostCatFragment()
+            replaceFragment(fragment)
+        }
         return binding.root
+    }
+    fun replaceFragment(someFragment:Fragment){
+        var binding: ActivityMainBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val transaction = requireActivity().supportFragmentManager.beginTransaction()
+        transaction.replace(binding.frameLayout.id, someFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 
 }
