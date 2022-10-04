1fae1442eea790f0168ff18d7dfd985805521695
HoimalangPoo <runjamankung@gmail.com>
2022-10-04 20:55:14 +0700

    ok

--- app/src/main/java/com/pac/kotlin_mobile/URL.kt
+++ app/src/main/java/com/pac/kotlin_mobile/URL.kt
@@ -1,5 +1,5 @@
 package com.pac.kotlin_mobile
 
 object URL {
-    var URL_API: String = "https://3522-180-183-70-210.jp.ngrok.io"
+    var URL_API: String = "https://3522-180-183-70-210.jp.ngrok.io/"
 }
