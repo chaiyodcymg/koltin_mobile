e4b24aac618d677a548008cfc16860dff07d81c0
Chaiyod Malingam <chaiyod.cymg@gmail.com>
2022-10-05 21:36:00 +0700

    commit

--- app/src/main/java/com/pac/kotlin_mobile/URL.kt
+++ app/src/main/java/com/pac/kotlin_mobile/URL.kt
@@ -2,8 +2,6 @@ package com.pac.kotlin_mobile
 
 object URL {
 
-
-    var URL_API: String = "https://3522-180-183-70-210.jp.ngrok.io/"
-
+    var URL_API: String = "https://3582-180-183-70-210.jp.ngrok.io/"
 }
 
