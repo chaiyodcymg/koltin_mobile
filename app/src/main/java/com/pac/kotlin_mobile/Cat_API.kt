542fe2a97cba5277be2d292b0e2fde5b5241c086
Waiwitz <itimmilyz@gmail.com>
2022-10-06 16:16:30 +0700

    pull pack

--- app/src/main/java/com/pac/kotlin_mobile/Cat_API.kt
+++ app/src/main/java/com/pac/kotlin_mobile/Cat_API.kt
@@ -1,10 +1,7 @@
 package com.pac.kotlin_mobile
 
 import retrofit2.Call
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface Cat_API {
     @GET("mypost")
@@ -15,6 +12,15 @@ interface Cat_API {
         @Query("data") data: String
 
         ): Call<List<Cat>>
+
+
+    @FormUrlEncoded /// Update
+    @PUT("updatePost/{id}")
+    fun updateMovie(
+        @Path("id") id: Int,
+        @Field("title") title: String,
+        @Field("year") year: Int,
+        @Field("img_movie") img_movie: String): Call<Cat>
 
     @DELETE("deletePost/{id}")
     fun deletePost(
--- app/src/main/java/com/pac/kotlin_mobile/EditPostActivity.kt
+++ app/src/main/java/com/pac/kotlin_mobile/EditPostActivity.kt
@@ -3,7 +3,7 @@ package com.pac.kotlin_mobile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class EditPost : AppCompatActivity() {
+class EditPostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_post)
